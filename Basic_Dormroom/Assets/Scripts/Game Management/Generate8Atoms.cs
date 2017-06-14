29e56e216281b11e4bb8b21d590a5fc3a7c4a4cb
davidnmora <dmora1@stanford.edu>
2017-06-14 15:46:48 -0700

    Dist spawning

--- Basic_Dormroom/Assets/Scripts/Game Management/Generate8Atoms.cs
+++ Basic_Dormroom/Assets/Scripts/Game Management/Generate8Atoms.cs
@@ -20,7 +20,7 @@ public class Generate8Atoms : MonoBehaviour {
 		for (int eCount = 0; eCount < 8; eCount++) {
 			// create an AtomPrefab with eCount electrons
 			atoms[eCount] = Instantiate(atomPrefab, transform) as AtomPrefabController;
-			atoms[eCount].transform.position = atomsStartPos.position + new Vector3(eCount * 1.2f, 0, 0);
+			atoms[eCount].transform.position = atomsStartPos.position + new Vector3(eCount * 0.7f, 0, 0);
 			atoms[eCount].numElectrons(eCount + 1);
 		}
 	}
